6fad7d155fc649f3cef78c33bdab8ed2c0ea20ff
benx <benx.shen@gmail.com>
2022-11-05 18:23:42 +0800

    chore: 重新命名檔案，讓測試較清楚

--- employees/src/test/java/com/howtodoinjava/employees/controllers/EmployeeControllerIT.java
+++ employees/src/test/java/com/howtodoinjava/employees/controllers/EmployeeControllerIT.java
@@ -4,15 +4,13 @@ import javax.validation.ValidationException;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.howtodoinjava.employees.model.Employee;
 
 @SpringBootTest
-public class IntegrationTests {
+public class EmployeeControllerIT {
 
 	@Autowired
 	EmployeeController employeeController;
--- employees/src/test/java/com/howtodoinjava/employees/controllers/EmployeeController_MockMvcTests.java
+++ employees/src/test/java/com/howtodoinjava/employees/controllers/EmployeeController_MockMvcTests.java
@@ -4,27 +4,21 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.howtodoinjava.employees.model.Employee;
 import com.howtodoinjava.employees.services.EmployeeService;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-public class StandaloneControllerTests2 {
+@WebMvcTest(EmployeeController.class)
+public class EmployeeController_MockMvcTests {
 
 	@MockBean
 	EmployeeService employeeService;
@@ -33,9 +27,9 @@ public class StandaloneControllerTests2 {
 	MockMvc mockMvc;
 
 	@Test
-	public void testfindAll() throws Exception {
+	public void testFindAll() throws Exception {
 		Employee employee = new Employee("Lokesh", "Gupta");
-		List<Employee> employees = Arrays.asList(employee);
+		List<Employee> employees = List.of(employee);
 
 		Mockito.when(employeeService.findAll()).thenReturn(employees);
 
