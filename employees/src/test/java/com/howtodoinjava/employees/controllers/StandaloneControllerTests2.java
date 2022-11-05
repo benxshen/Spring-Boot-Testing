62d8e74b7d4b3d7b35a1e66388fc4f70b9e193f0
Ben <benx.shen@gmail.com>
2022-11-05 16:00:21 +0800

    add new test

---
+++ employees/src/test/java/com/howtodoinjava/employees/controllers/StandaloneControllerTests2.java
@@ -0,0 +1,48 @@
+package com.howtodoinjava.employees.controllers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.howtodoinjava.employees.model.Employee;
+import com.howtodoinjava.employees.services.EmployeeService;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class StandaloneControllerTests2 {
+
+	@MockBean
+	EmployeeService employeeService;
+
+	@Autowired
+	MockMvc mockMvc;
+
+	@Test
+	public void testfindAll() throws Exception {
+		Employee employee = new Employee("Lokesh", "Gupta");
+		List<Employee> employees = Arrays.asList(employee);
+
+		Mockito.when(employeeService.findAll()).thenReturn(employees);
+
+		mockMvc.perform(get("/employee"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$", Matchers.hasSize(1)))
+				.andExpect(jsonPath("$[0].firstName", Matchers.is("Lokesh")));
+	}
+
+}
